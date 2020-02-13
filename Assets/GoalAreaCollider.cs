14a3be2bc8637114186650b55658e12150301a3e
shota_1011 <sky23buriki@gmail.com>
2020-02-13 12:45:17 +0900

    Add Goal

---
+++ Assets/GoalAreaCollider.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoalAreaCollider : MonoBehaviour
+{
+    //ゴール時に出てくるもの
+    [SerializeField] private  GameObject GoalNameObj;
+    //
+    [SerializeField] private float Timer;
+    private float counter;
+    private bool GoalFlag;
+
+
+    //プレイヤー判定
+    void Awake()
+    {
+        //ゴール時に出てくるものを非表示
+        GoalNameObj.SetActive(false);
+        counter = 0;
+        GoalFlag = false;
+    }
+
+
+    void Update()
+    {
+        if(GoalFlag)
+        {
+            counter++;
+        }
+
+        if(GoalFlag && counter <= Timer)
+        {
+            //シーンの再読み込み
+            Scene loadScene = SceneManager.GetActiveScene();
+            SceneManager.LoadScene(loadScene.name);
+        }
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (GoalFlag == true) return;
+
+        if(collision.gameObject.tag == "Player")
+        {//プレイヤーが衝突
+            GoalNameObj.SetActive(true);
+            GoalFlag = true;
+        }
+    }
+}
